4c59103693434166274e5abe5813def275dde696
ivangeorgiev181@gmail.com <ivangeorgiev181@gmail.com>
2026-09-17 22:46:09 +0300

    basic model for the book for db

---
+++ Readers/Data/Book.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Readers.Data
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        public string PublishingHouse { get; set; } = null!;
+
+        [Required]
+        public string Ganre { get; set; } = null!;
+
+        [Required]
+        public int? YearPublished { get; set; } = null!;
+
+        [Required]
+        public int? pages { get; set; } = null!;
+
+        public virtual int Comments { get; set; } = 0;
+
+        public virtual int likes { get; set; } = 0;
+    }
+}
