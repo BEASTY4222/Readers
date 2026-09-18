3d205aa2e85ac917d7d346c2e6ec41c0eedfb64f
ivangeorgiev181@gmail.com <ivangeorgiev181@gmail.com>
2026-09-18 14:45:28 +0300

    a simple like model because I realized having a simple int in the model for the book is just a counter

---
+++ Readers/Data/DataModels/Like.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Readers.Data.DataModels
+{
+    public class Like
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int BookId { get; set; }
+        public Book book { get; set; } = null!;
+
+
+        public string UserId { get; set; } = null!;
+        public ApplicationUser user { get; set; } = null!;
+    }
+}
