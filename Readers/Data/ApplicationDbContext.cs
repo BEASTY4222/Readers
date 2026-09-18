88bfafa10c0cce8f034691dd29aa61642534c63b
ivangeorgiev181@gmail.com <ivangeorgiev181@gmail.com>
2026-09-18 18:11:08 +0300

    configured the db context to include all the tables because they all share relationships Books Comments Likes and users but the dbSet Users is inherited

--- Readers/Data/ApplicationDbContext.cs
+++ Readers/Data/ApplicationDbContext.cs
@@ -1,9 +1,24 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Readers.Data.DataModels;
 
 namespace Readers.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Like>()
+                .HasIndex(l => new { l.UserId, l.BookId })
+                .IsUnique();
+        }
     }
+
 }
+
