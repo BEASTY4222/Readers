64d9e457d8f6390785c44e3946ddf624b042cd36
ivangeorgiev181@gmail.com <ivangeorgiev181@gmail.com>
2026-09-18 14:46:06 +0300

    same thing with the like a int comment field is just a counter so seperate table connected to both ends with book and user

---
+++ Readers/Data/DataModels/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Readers.Data.DataModels
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+    
+        public int BookId { get; set; }
+        public Book Book { get; set; } = null!;
+
+        public string UserId { get; set; } = null!;
+        public ApplicationUser User { get; set; } = null!;
+    }
+}
