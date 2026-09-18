60e301b3a48d27d447d4e9c75b2b48e1e8fa24d3
ivangeorgiev181@gmail.com <ivangeorgiev181@gmail.com>
2026-09-18 14:47:50 +0300

    I updated the Book model with the like and comment tabel to fix previous mistakes

---
+++ Readers/Data/DataModels/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using Common;
+
+namespace Readers.Data.DataModels
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(EntityDataLimits.TitleMaxLength, MinimumLength = EntityDataLimits.TitleMinLenght)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string? CoverImagePath { get; set; }
+
+        [Required]
+        [StringLength(EntityDataLimits.AuthorMaxLength, MinimumLength = EntityDataLimits.AuthorMinLenght)]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        [StringLength(EntityDataLimits.PublishingHouseMaxLength, MinimumLength = EntityDataLimits.PublishingHouseMinLenght)]
+        public string PublishingHouse { get; set; } = null!;
+
+        [Required]
+        [StringLength(EntityDataLimits.GanreMaxLength, MinimumLength = EntityDataLimits.GanreMinLenght)]
+        public string Ganre { get; set; } = null!;
+
+        [Required]
+        [Range(EntityDataLimits.YearPublishedMinLenght, EntityDataLimits.YearPublishedMaxLength)]
+        public int YearPublished { get; set; } = 0;
+
+        [Required]
+        [Range(EntityDataLimits.PagesMinLenght, EntityDataLimits.PagesMaxLength)]
+        public int pages { get; set; } = 0;
+
+        public ICollection<Like> Likes { get; set; } = new List<Like>();
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
