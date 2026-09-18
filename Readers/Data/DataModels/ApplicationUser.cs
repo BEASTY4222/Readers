1a2b733a90d970f8eec017987bf33439f2851144
ivangeorgiev181@gmail.com <ivangeorgiev181@gmail.com>
2026-09-18 14:47:23 +0300

    A user table mostly rellying on convetion I just added the lists for the likes and comments because they are also connected to the users

---
+++ Readers/Data/DataModels/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Readers.Data.DataModels
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+        public ICollection<Like> Likes { get; set; } = new List<Like>();
+    }
+}
