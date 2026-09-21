893854bd8594a054574aaa184f3192b53f8fe788
Ivan Georgiev School <ivangeorgiev181@gmail.com>
2026-09-21 09:07:23 +0300

    added routing straight to Books "/Books"

--- Readers/Controllers/BooksController.cs
+++ Readers/Controllers/BooksController.cs
@@ -4,6 +4,7 @@ namespace Readers.Controllers
 {
     public class BooksController : Controller
     {
+        [Route("/Books")]
         public IActionResult Index()
         {
             return View();
