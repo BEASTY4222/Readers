6a375c1ff0779c8d8521de1d6361097009775c03
Ivan Georgiev School <ivangeorgiev181@gmail.com>
2026-09-23 09:46:48 +0300

    I realized there would be too much logic in the view so moving it up to a controller with a modelView

---
+++ Readers/ViewModels/LanguageSwitcherViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Readers.Models.ViewModels
+{
+    public class LanguageSwitcherViewModel
+    {
+        public string CurrentCultureName { get; set; } = string.Empty;
+        public string ReturnUrl { get; set; } = string.Empty;
+        public List<CultureOption> Cultures { get; set; } = new();
+    }
+
+    public class CultureOption
+    {
+        public string Name { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
+    }
+}
