6670d23d51c696d83889420d4e4dd606b1e19f72
Ivan Georgiev School <ivangeorgiev181@gmail.com>
2026-09-24 09:34:09 +0300

    made an author data model

---
+++ Readers/Data/DataModels/Author.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using Common;
+
+namespace Readers.Data.DataModels
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Range(EntityDataLimits.NameMinLenght, EntityDataLimits.NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Range(EntityDataLimits.CountryMinLenght, EntityDataLimits.CountryMaxLength)]
+        public string? country { get; set; }
+
+        [ForeignKey(nameof(Book))]
+        public int BookId { get; set; }
+        public ICollection<Book> Books { get; set; } = null!;
+    }
+}
