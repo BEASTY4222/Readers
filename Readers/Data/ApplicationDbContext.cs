132dbbba6c9978a69cc06204b90dfebd4747ad8b
Ivan Georgiev School <ivangeorgiev181@gmail.com>
2026-09-25 10:28:29 +0300

    added the db contex for Authors table

--- Readers/Data/ApplicationDbContext.cs
+++ Readers/Data/ApplicationDbContext.cs
@@ -9,6 +9,7 @@ namespace Readers.Data
         public DbSet<Book> Books { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Like> Likes { get; set; }
+        public DbSet<Author> Authors { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
