e71fd8a9781aecd41779f222d6192c48d250c733
ivangeorgiev181@gmail.com <ivangeorgiev181@gmail.com>
2026-09-26 14:19:47 +0300

    the last migration before the seen so the database is up-to-data

---
+++ Readers/Data/Migrations/20260926111652_MigrationBeforeFirstSeed.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Readers.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationBeforeFirstSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "pages",
+                table: "Books",
+                newName: "Pages");
+
+            migrationBuilder.RenameColumn(
+                name: "country",
+                table: "Authors",
+                newName: "Country");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoverImagePath",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Pages",
+                table: "Books",
+                newName: "pages");
+
+            migrationBuilder.RenameColumn(
+                name: "Country",
+                table: "Authors",
+                newName: "country");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoverImagePath",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
